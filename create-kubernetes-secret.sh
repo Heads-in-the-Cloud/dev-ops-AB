c12e2bec219b3a0bb5c5280404fd468ae935565d
Austin Baugh <austin.baugh@smoothstack.com>
2021-11-19 11:44:56 -0800

    Adding current kubernetes work

--- create-kubernetes-secret.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-kubectl create secret generic db-user-creds --from-file=./mysql_username.txt --from-file=./mysql_password.txt
-kubectl create secret generic db-secrets --from-file=./mysql_username.txt --from-file=./mysql_password.txt --from-file=./mysql_root_password.txt
