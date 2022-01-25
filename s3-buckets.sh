8e3ce96710c7a5cb1471aa49668fdf247b1b9251
Austin Baugh <austin.baugh@smoothstack.com>
2022-01-24 22:06:12 -0800

    Appearance changes for readability

--- s3-buckets.sh
+++ s3-buckets.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 region=us-west-2
-db_s3_bucket=db-init-ab
+s3_bucket=utopia-ab
 #tf_s3_bucket=tf-plans-ab
 
-aws s3api create-bucket \
-  --bucket $db_s3_bucket \
-  --region $region \
-  --create-bucket-configuration LocationConstraint=$region
+#aws s3api create-bucket \
+#  --bucket $s3_bucket \
+#  --region $region \
+#  --create-bucket-configuration LocationConstraint=$region
 
-aws s3 cp mysql/*.sql s3://$db_s3_bucket
+aws s3 cp --recursive mysql/ s3://$s3_bucket
 
 #aws s3api create-bucket \
 #  --bucket $tf_s3_bucket \
