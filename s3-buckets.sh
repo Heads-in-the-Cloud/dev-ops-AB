f9ba74b8ee53a4d76edbc0ac912f86ed7a071245
Austin Baugh <austin.baugh@smoothstack.com>
2022-01-25 19:24:23 -0800

    Moved initials to prefix project id

--- s3-buckets.sh
+++ s3-buckets.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 region=us-west-2
-s3_bucket=utopia-ab
+s3_bucket=ab-utopia
 #tf_s3_bucket=tf-plans-ab
 
-#aws s3api create-bucket \
-#  --bucket $s3_bucket \
-#  --region $region \
-#  --create-bucket-configuration LocationConstraint=$region
+aws s3api create-bucket \
+  --bucket $s3_bucket \
+  --region $region \
+  --create-bucket-configuration LocationConstraint=$region
 
-aws s3 cp --recursive mysql/ s3://$s3_bucket
+aws s3 cp --recursive mysql/ s3://$s3_bucket/mysql
 
 #aws s3api create-bucket \
 #  --bucket $tf_s3_bucket \
