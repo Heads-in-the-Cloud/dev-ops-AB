f767d01554870cfba0a79284d7b792cb04bf28bb
Austin Baugh <austin.baugh@smoothstack.com>
2022-01-25 18:00:37 -0800

    Updated subnet id output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,10 @@ output "vpc_id" {
   value = module.networks.vpc_id
 }
 output "private_subnets" {
-  value = module.networks.private_subnet_ids
+  value = module.networks.subnet_ids.private
 }
 output "public_subnets" {
-  value = module.networks.public_subnet_ids
+  value = module.networks.subnet_ids.public
 }
 output "alb_id" {
   value = module.networks.alb_id
