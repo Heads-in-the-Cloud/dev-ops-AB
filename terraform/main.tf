fb21cf662d338bd52dc6d04d20ccd02e2db44938
Austin Baugh <austin.baugh@smoothstack.com>
2022-01-27 01:56:27 -0800

    Changed default cidr block

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ data "aws_secretsmanager_secret_version" "default" {
 
 locals {
   secrets = jsondecode(data.aws_secretsmanager_secret_version.default.secret_string)
-  vpc_cidr_block     = "10.6.0.0/16"
+  vpc_cidr_block     = "10.0.0.0/16"
   subnet_cidr_blocks = {
     private = ["10.0.0.0/24", "10.0.1.0/24"]
     public  = ["10.0.2.0/24", "10.0.3.0/24"]
