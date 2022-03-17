0923dcc8c9ac6ea41f34dc82358e2f3babefb202
Austin Baugh <austin@serene.cafe>
2022-03-17 15:04:02 -0700

    Changed shell environment var expanssion

--- eks/aws-auth.sh
+++ eks/aws-auth.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 kubectl get configmap/aws-auth -n kube-system -o yaml |
-  sed "0,/data:/s//data: \
+  sed '0,/data:/s//data: \
   mapUsers: | \
-    \- userarn: arn:aws:iam::$AWS_ACCOUNT_ID:user\/$IAM_USERNAME \
-      username: $IAM_USERNAME \
+    \- '"userarn: arn:aws:iam::$AWS_ACCOUNT_ID:user\/$IAM_USERNAME \
+      username: $IAM_USERNAME"' \
       groups: \
-      \- system:masters/" > configmap.yaml && kubectl apply -f configmap.yaml
+      \- system:masters/' > configmap.yaml && kubectl apply -f configmap.yaml
