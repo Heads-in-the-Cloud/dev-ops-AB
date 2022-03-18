f06a0e1c1f248472af1602a8631dd2e7f570734b
Austin Baugh <austin@serene.cafe>
2022-03-17 20:39:11 -0700

    Moved user role creation to sql file

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -11,22 +11,19 @@
 yum update -y
 yum install -y mysql
 
-aws s3 cp s3://${s3_bucket}/schema.sql .
+aws s3 cp --recursive s3://${s3_bucket}/mysql .
 
-# Provision RDS instance with empty DB, user roles, and the microservice DB user
 mysql -h "${db_host}" -u "${db_root_username}" -p"${db_root_password}" << EOF
+
 $(cat schema.sql)
+$(cat data.sql)
 
-LOCK TABLES `user_role` WRITE;
-INSERT INTO `user_role` VALUES (3,'Admin'),(1,'Employee'),(2,'Traveler');
-UNLOCK TABLES;
-
--- Add microservice user
-CREATE USER '${db_username}'@'%' IDENTIFIED WITH mysql_native_password BY '${db_password}';
+-- Add microservices user
+CREATE USER '${db_username}'@'%' IDENTIFIED BY '${db_password}';
 GRANT SELECT, INSERT, UPDATE, DELETE ON utopia.* TO '${db_username}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-rm schema.sql
+rm *.sql
 
 poweroff
