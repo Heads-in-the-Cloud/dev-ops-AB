a8d9296c91afda901afa4df67508ababb00c2f8a
Austin Baugh <austin@serene.cafe>
2022-03-22 23:23:25 -0700

    Changed db provisioning instruction order

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# The template variables are assumed to be set:
+# Template variables assumed to be set:
 # - s3_bucket
 # - db_host
 # - db_root_username
 # - db_root_password
-# - max_connections
 # - db_username
 # - db_password
+# - max_connections
 
 yum update -y
 yum install -y mysql
@@ -17,14 +17,15 @@ aws s3 cp --recursive s3://${s3_bucket}/mysql .
 mysql -h "${db_host}" -u "${db_root_username}" -p"${db_root_password}" << EOF
 
 $(cat schema.sql)
-$(cat data.sql)
-
-SET GLOBAL max_connections='${max_connections}';
 
 -- Add microservices user
 CREATE USER '${db_username}'@'%' IDENTIFIED BY '${db_password}';
 GRANT SELECT, INSERT, UPDATE, DELETE ON utopia.* TO '${db_username}'@'%';
 FLUSH PRIVILEGES;
+
+SET GLOBAL max_connections='${max_connections}';
+
+$(cat data.sql)
 EOF
 
 rm *.sql
