b34f802c57a7f9aaf438848a05b2e3c8d6ea4a78
Austin Baugh <austin@serene.cafe>
2022-04-02 23:32:07 -0700

    echo env vars in aws auth script

--- eks/aws-auth.sh
+++ eks/aws-auth.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+echo $AWS_ACCOUNT_ID
+echo $IAM_USERNAME
+
 kubectl get configmap/aws-auth -n kube-system -o yaml |
-sed '0,/data:/s//data: \
+  sed '0,/data:/s//data: \
   mapusers: | \
     \- '"userarn: arn:aws:iam::$AWS_ACCOUNT_ID:user\/$IAM_USERNAME"' \
       '"username: $IAM_USERNAME"' \
       groups: \
       \- system:masters/' |
-kubectl apply -f -
+  kubectl apply -f -
