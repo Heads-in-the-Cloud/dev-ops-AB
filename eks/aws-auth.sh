41fe9a801840b065faee5331dff605c550d607de
Austin Baugh <austin@serene.cafe>
2022-04-02 21:59:45 -0700

    Moved environment var assignments back into script

--- eks/aws-auth.sh
+++ eks/aws-auth.sh
@@ -2,9 +2,9 @@
 
 kubectl get configmap/aws-auth -n kube-system -o yaml |
   sed '0,/data:/s//data: \
-  mapUsers: | \
-    \- '"userarn: arn:aws:iam::$AWS_ACCOUNT_ID:user\/$IAM_USERNAME"' \
-      '"username: $IAM_USERNAME"' \
+  mapusers: | \
+    \- '"userarn: arn:aws:iam::$aws_account_id:user\/$iam_username"' \
+      '"username: $iam_username"' \
       groups: \
       \- system:masters/' |
   kubectl apply -f -
